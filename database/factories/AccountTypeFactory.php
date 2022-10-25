be5d68449dbb47240351076018b5782b21f7ba85
christophegourmand <christophe.gourmand@gmail.com>
2022-10-25 21:56:02 +0100

    fill seeder for AccountType

--- database/factories/AccountTypeFactory.php
+++ database/factories/AccountTypeFactory.php
@@ -17,7 +17,7 @@ class AccountTypeFactory extends Factory
     public function definition()
     {
         return [
-            //
+            // 'name' =>
         ];
     }
 }
--- database/seeders/AccountTypeSeeder.php
+++ database/seeders/AccountTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\AccountType;
 
 class AccountTypeSeeder extends Seeder
 {
@@ -14,6 +15,11 @@ class AccountTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        AccountType::factory()->create( ['name' => 'individuel'] );
+        AccountType::factory()->create( ['name' => 'épargne'] );
+        AccountType::factory()->create( ['name' => 'titre'] );
+        AccountType::factory()->create( ['name' => 'à terme'] );
+        AccountType::factory()->create( ['name' => 'join'] );
+        AccountType::factory()->create( ['name' => 'indivis'] );
     }
 }

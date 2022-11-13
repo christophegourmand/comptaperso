d39fdc976b00caf26670a8b4f4a7b56b3f560cb8
christophegourmand <christophe.gourmand@gmail.com>
2022-11-13 17:28:35 +0100

    (bankAccount): move JS about color picker in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,4 +6,14 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// import './colorPickerToIcon';
+// ============================================================================
+// SECTION : transmit color from picker to icon in form to create bank account
+// ============================================================================
+
+let colorPickerInput = document.getElementById('bank_account_icon_color_hexa');
+let iconsFormFieldset = document.getElementById('bank_account_icon');
+
+colorPickerInput.addEventListener('change', event =>{
+    let colorHexaCode = event.target.value;
+    iconsFormFieldset.style.color = colorHexaCode;
+});
--- resources/js/colorPickerToIcon.js
+++
@@ -1,9 +0,0 @@
-// console.log('MODULE LOADED: colorPIckerToIcon.js');
-
-let colorPickerInput = document.getElementById('bank_account_icon_color_hexa');
-let iconsFormFieldset = document.getElementById('bank_account_icon');
-
-colorPickerInput.addEventListener('change', event =>{
-    let colorHexaCode = event.target.value;
-    iconsFormFieldset.style.color = colorHexaCode;
-});

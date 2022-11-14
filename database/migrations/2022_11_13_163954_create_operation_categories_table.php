e03c91b1588488d222084acb12d55727037de3fb
christophegourmand <christophe.gourmand@gmail.com>
2022-11-14 18:34:46 +0100

    (Operation): add fields

--- database/migrations/2022_11_13_163954_create_operation_categories_table.php
+++ database/migrations/2022_11_13_163954_create_operation_categories_table.php
@@ -17,6 +17,14 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->string('name', 50);
+            $table->boolean('is_visible')->default(true);
+            $table->boolean('is_counted_in_balance')->default(true);
+            $table->bigInteger('icon_id')->unsigned()->nullable();
+            $table->string('icon_color_hexa', 7)->nullable();
+            $table->double('monthly_limit' , 24 , 7)->nullable()
+                ->comment('Use that field as a budget limit (to stay under) for the month.');
+            $table->double('annual_limit' , 24 , 7)->nullable()
+                ->comment('Use that field as a budget limit (to stay under) for the year.');
             $table->timestamps();
 
             $table->index(['user_id','name']);
@@ -27,7 +35,9 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
-
+            $table->foreign('icon_id')->references('id')->on('icons')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
         });
     }
 
--- database/migrations/2022_11_13_165055_create_operations_table.php
+++ database/migrations/2022_11_13_165055_create_operations_table.php
@@ -15,7 +15,52 @@ return new class extends Migration
     {
         Schema::create('operations', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('bank_account_id')->unsigned();
+            $table->bigInteger('operation_category_id')->unsigned()->nullable();
+            $table->date('date_of_operation')->default( now() );
+            $table->date('date_of_value')->nullable();
+            $table->double('amount' , 24 , 8);
+            $table->string('label' , 200);
+            $table->mediumText('description')->nullable();
+            $table->bigInteger('thirdparty_id')->unsigned()->nullable();
+            $table->bigInteger('operation_type_id')->unsigned()->nullable();
+            $table->bigInteger('operation_status_id')->unsigned()->nullable();
+            $table->mediumText('comment')->nullable();
+            $table->bigInteger('recurring_id')->unsigned()->default(1)->nullable()
+                ->comment('Allow to choose how an operation is recurring (default on 1 mean unique)');
+            $table->tinyInteger('recurring_x_days_interval', false , true)->nullable()->comment('min:1 , max: 365 days');
+            $table->tinyInteger('recurring_j_day_of_the_month', false , true)->nullable()->comment('must be from 1 to 31');
+            $table->tinyInteger('recurring_n_months_interval', false , true)->nullable()->comment(' from 1 to 48 months');
+            $table->boolean('is_reconciliated')->default(false);
+            $table->boolean('is_forecast')->nullable()
+                ->comment('Indicate if this operation is forecast or theoric or prevision == didnt really happen.');
+            $table->bigInteger('source_operation_id')->unsigned()->nullable()
+                ->comment('When an operation is a copy from another');
             $table->timestamps();
+        });
+
+        Schema::table('operations', function(Blueprint $table) {
+            $table->foreign('bank_account_id')->references('id')->on('bank_accounts')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreign('operation_category_id')->references('id')->on('operation_categories')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreign('thirdparty_id')->references('id')->on('thirdparties')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreign('operation_type_id')->references('id')->on('thirdparty_comments')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreign('operation_status_id')->references('id')->on('operation_statuses')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreign('recurring_id')->references('id')->on('operation_recurrings')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreign('source_operation_id')->references('id')->on('operations')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
         });
     }
 
